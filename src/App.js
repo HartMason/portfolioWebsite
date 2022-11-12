6f16381e6377c302af2b31be79fd409bf6babc1d
HartMason <hartmason85@gmail.com>
2022-11-12 15:24:02 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return <div>
     <h1>Portfolio Website</h1>
-    <p>My portfolio website is built with React JS and material UI</p>
-    <p>It features different projects I have completed along my journey of becoming a software engineer</p>
+    <p>My portfolio website is built with React, Java Script, CSS, and material UI</p>
+    <p>It features different front-end projects I have completed along my journey of becoming a software engineer</p>
   </div>;
 }
 
